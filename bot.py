bf3829894e2a2489f2b4f3477477db018891d558
DiegoGonL <dgonzl05@estudiantes.unileon.es>
2020-07-04 12:41:18 +0200

    Created function change_username in bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ import logging
 from config import TOKEN
 from keyboards import main_keyboard
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
+from telegram import ChatAction
 
 updater = Updater(token=TOKEN, use_context=True)
+users_dni = {}
 
 
 def start(update, context):
@@ -11,7 +13,6 @@ def start(update, context):
 
 
 def logger(update, context):
-
     if update.effective_chat.type != 'private':
 
         path = "logs/%s.txt" % update.effective_chat.id
@@ -19,8 +20,8 @@ def logger(update, context):
         '''
         In case the folder 'logs' doesn't exists, you need to create it,
         or change the path to wherever you want to save the logs 
-        
         '''
+
         try:
             f = open(path, "a")
         except FileNotFoundError:
@@ -28,21 +29,39 @@ def logger(update, context):
 
         if update.effective_user.username is None:
             update.message.reply_text(
-                text=update.effective_user.first_name + ', necesito que establezcas un nombre de usuario en telegram'
+                text=update.effective_user.first_name + ', I need you to set a username in the telegram settings'
             )
         else:
+
+            if update.effective_user.username in users_dni:
+                username = users_dni[update.effective_user.username]
+            else:
+                username = update.effective_user.username
+
             f.write("[" + update.message.date.strftime("%d/%m/%Y, %H:%M:%S") + "] " +
-                    update.effective_user.username + ": " + update.message.text + "\n")
+                    username + ": " + update.message.text + "\n")
 
         f.close()
 
 
+def change_username(update, context):
+    """
+    TODO: ConversationHandler in main to be able to receive the new username as update.message.text
+    """
+    users_dni["%s" % update.effective_user.username] = "NewUsername"
+
+
 def main():
+    """
+    TODO: Save the users_dni dictionary in a file, charge it at the begging and update it each time change_username is accessed
+    """
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     dispatcher = updater.dispatcher
     start_handler = CommandHandler('start', start)
     logger_handler = MessageHandler(Filters.text & (~Filters.command), logger)
+    username_handler = CommandHandler('username', change_username)
 
+    dispatcher.add_handler(username_handler)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(logger_handler)
     updater.start_polling()
